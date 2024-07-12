7da569d18b564dd548e871f6ea9e60f8d7a566a1
Carly Mead <meadcarly16@gmail.com>
2024-07-12 08:31:25 -0600

    Changed explicit typing to inferred typing, used string interpolation in creating a ternary operator

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Given the following code, change the code to implement:
+            // inferred typing
+            // string interpolation
+            // and the ternary operator
+            var answer = 4;
+            string response;
+
+            Console.WriteLine(answer < 9 ? $"{answer} is less than nine": $"{answer} is greater than or equal to nine");
+            /*if (answer < 9) 
+            {
+                response = answer + " is less than nine";
+            }
+            else
+            {
+                response = answer + " is greater than or equal to nine";
+            }*/
         }
     }
 }
